1959f5e31f0277a1eeddca7a703a6285576afe33
sohbr <sohnbrian@gmail.com>
2018-04-26 20:21:13 -0700

    add function to grab embed code

---
+++ getUrlParams.js
@@ -0,0 +1,10 @@
+function getUrlParams(url) {
+  let queryString;
+  if (url) {
+    queryString = url.split('=');
+    queryString = queryString[queryString.length- 1];
+  }
+
+  return queryString;
+
+}
